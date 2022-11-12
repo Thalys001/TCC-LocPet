d7981a9f3c4237d59761efbf28e190d91510d2df
Thalys Leite <thalyssouza708@gmail.com>
2022-11-12 06:48:41 -0300

    feat(menu/camera): add expo camera

--- src/pages/Camera/index.js
+++ src/pages/Camera/index.js
@@ -1,22 +1,61 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native'
+import { Camera } from 'expo-camera'
 
-export default function Camera() {
+export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null)
+  const [camera, setCamera] = useState(null)
+  const [image, setImage] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  useEffect(() => {
+    ;(async () => {
+      // const cameraStatus = await Camera.requestPermissionsAsync()
+      setHasCameraPermission(cameraStatus.status === 'granted')
+    })()
+  }, [])
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null)
+      setImage(data.uri)
+    }
+  }
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}></Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={ref => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={'1:1'}
+        />
+      </View>
+      <Button
+        title="virar camera"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          )
+        }}
+      ></Button>
+      <Button title="tirar foto" onPress={() => takePicture()} />
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flez: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'center',
+    marginBottom: '0%'
   },
-  text: {
-    fontSize: 25,
-    fontWeight: 'bold'
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1
   }
 })
