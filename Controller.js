2ed4e07ca84b5f55bbb4a5ce4a4ebe23cd315363
HeannaReis <heannareis@gmail.com>
2022-11-12 08:22:32 -0300

    feat(Server/Back-End): add server back end

---
+++ Controller.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const cors=require('cors');
+const bodyParse=require('body-parser');
+
+const app=express();
+app.use(cors());
+app.use(bodyParse.urlencoded({ extended: false }));
+
+app.get('/',(req, res)=>{
+    res.send(' LocPet Back End, está no Ar. Chupa!!! ')
+});
+
+let port=process.env.PORT || 3000;
+app.listen(port,(req,res)=> {
+    console.log (' LocPet Back-End Rodando Porra !!! ')
+
+});
